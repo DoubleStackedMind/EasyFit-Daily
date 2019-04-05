2d19f8b24075eb836b4040e07fdd1f3c2ad08a65
ransom <ahmedamine.benmimoun@esprit.tn>
2019-04-05 16:09:50 +0100

    Initial Commit

---
+++ EasyFit Daily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyFit Daily
+//
+//  Created by Ahmed amine on 4/5/19.
+//  Copyright © 2019 Ahmed amine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
